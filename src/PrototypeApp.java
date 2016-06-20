e2322861bde2c081cfa55b5ca7f2d527c176bb7f
archMcMilan <artempryzhkov@gmail.com>
2016-06-20 19:39:44 +0300

    Pattern realization

---
+++ src/PrototypeApp.java
@@ -0,0 +1,65 @@
+/**
+ * Created by Artem on 20.06.16.
+ */
+public class PrototypeApp {
+    public static void main(String[] args) {
+        Human original=new Human(18,"Vasya");
+        System.out.println(original);
+
+        Human copy= (Human) original.copy();
+        System.out.println(copy);
+
+        HumanFactory factory=new HumanFactory(copy);
+        Human h1=factory.makeCopy();
+        System.out.println(h1);
+
+        factory.setProperty(new Human(30,"Valeria"));
+        Human h2=factory.makeCopy();
+        System.out.println(h2);
+    }
+}
+
+interface Copyable{
+    Object copy();
+}
+
+class Human implements Copyable{
+    int age;
+    String name;
+
+    public Human(int age, String name) {
+        this.age = age;
+        this.name = name;
+    }
+
+    @Override
+    public Object copy() {
+        Human copy=new Human(age,name);
+        return copy;
+    }
+
+    @Override
+    public String toString() {
+        return "Human{" +
+                "age=" + age +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
+
+class HumanFactory{
+    Human human;
+
+    public HumanFactory(Human human) {
+        setProperty(human);
+    }
+
+    public void setProperty(Human human){
+        this.human=human;
+    }
+
+    Human makeCopy(){
+        return (Human)human.copy();
+    }
+
+}
